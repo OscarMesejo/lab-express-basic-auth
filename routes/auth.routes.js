e7e9ed2d03edfc159cd8983e2e6add6266f9acd1
Oscar Mesejo <oscar.mesejo@gmail.com>
2023-08-09 15:46:09 +0200

    doing iteration 2

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,11 +2,27 @@
  
 const { Router } = require('express');
 const router = new Router();
+const mongoose = require('mongoose');
+
+
  
 // GET route ==> to display the signup form to users
 
 router.get('/signup', (req, res) => res.render('auth/signup'));
+
+router.get('/login', (req, res) => res.render('auth/login'));
  
 // POST route ==> to process form data
+
+router.post('/signup', (req, res, next) => {
+    const { username, email, password } = req.body;
+   
+    // make sure users fill all mandatory fields:
+    if (!username || !email || !password) {
+      res.render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your username, email and password.' });
+      return;
+    }
+    
+})
  
 module.exports = router;
