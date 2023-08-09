e092330c212185a7e7d72ceb667a6bc45dddecf5
Oscar Mesejo <oscar.mesejo@gmail.com>
2023-08-09 18:05:22 +0200

    iteration 2

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,6 +2,8 @@
  
 const { Router } = require('express');
 const router = new Router();
+const bcryptjs = require('bcryptjs');
+const saltRounds = 10;
 const mongoose = require('mongoose');
 
 
@@ -11,7 +13,9 @@ const mongoose = require('mongoose');
 router.get('/signup', (req, res) => res.render('auth/signup'));
 
 router.get('/login', (req, res) => res.render('auth/login'));
- 
+
+router.get('/userProfile', (req, res) => res.render('users/user-profile'));
+
 // POST route ==> to process form data
 
 router.post('/signup', (req, res, next) => {
@@ -22,7 +26,29 @@ router.post('/signup', (req, res, next) => {
       res.render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your username, email and password.' });
       return;
     }
-    
+    bcryptjs
+    .genSalt(saltRounds)
+    .then(salt => bcryptjs.hash(password, salt))
+    .then(hashedPassword => {
+      console.log(`Password hash: ${hashedPassword}`);
+    })
+    .then(userFromDB => {
+      console.log('Newly created user is: ', userFromDB);
+      // res.redirect('/userProfile');
+    })
+    .catch(error =>{
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(500).render('auth/signup', { errorMessage: error.message });
+    } else {
+     next(error);
+    }
+});
 })
+
+
+
+    
+    
+
  
 module.exports = router;
